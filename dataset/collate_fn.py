077a62488b85452ebbbeced28188bbad34d10180
akram kohansal <akram.kohansal@gmail.com>
2019-01-24 12:37:05 +0100

    change loader

--- dataset/collate_fn.py
+++ dataset/collate_fn.py
@@ -5,11 +5,14 @@ def text_collate(batch):
     img = list()
     seq = list()
     seq_len = list()
+    print("inja")
     for sample in batch:
+        print("inja batch")
         img.append(torch.from_numpy(sample["img"].transpose((2, 0, 1))).float())
         seq.extend(sample["seq"])
         seq_len.append(sample["seq_len"])
     img = torch.stack(img)
+    print(len(img))
     seq = torch.Tensor(seq).int()
     seq_len = torch.Tensor(seq_len).int()
     batch = {"img": img, "seq": seq, "seq_len": seq_len}
